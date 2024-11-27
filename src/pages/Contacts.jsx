33cbd100692ee3266259a4ab393e98204793fb34
mk-saadi <mksaadi820@gmail.com>
2024-11-28 05:12:36 +0600

    xl device responsive

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,32 +2,52 @@ import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { Forward } from "lucide-react";
 import { Fade, Slide } from "react-awesome-reveal";
+import { useToast } from "react-toast-master";
 
 const Contacts = () => {
 	const form = useRef();
+	const { toastMaster } = useToast();
 
 	const sendEmail = (e) => {
 		e.preventDefault();
 
-		// emailjs
-		// .sendForm(
-		// 	"service_at3kvap",
-		// 	// "template_rrsov4c",
-		// 	"template_du1zsn7",
-		// 	form.current,
-		// 	"AWTDDzD1prNjlGP-F"
-		// )
-		emailjs.send("service_at3kvap", "template_du1zsn7", form.current, "AWTDDzD1prNjlGP-F").then(
-			(result) => {
-				console.log(result.text);
-				alert("email sent successfully");
-				e.target.reset();
-			},
-			(error) => {
-				console.log(error.text);
-				alert("please try again");
-			}
-		);
+		toastMaster({
+			type: "loadingDark",
+			message: "Sending email",
+			footer: "Please wait, it might take a few seconds",
+			bg: "white",
+			position: "bottomLeft",
+			transition: "top",
+		});
+
+		emailjs
+			.send(
+				import.meta.env.VITE_SERVICE,
+				import.meta.env.VITE_TEMPLATE,
+				form.current,
+				import.meta.env.VITE_ID
+			)
+			.then(
+				(result) => {
+					e.target.reset();
+					toastMaster({
+						type: "successDark",
+						message: "Email sent successfully",
+						bg: "white",
+						position: "bottomLeft",
+						transition: "top",
+					});
+				},
+				(error) => {
+					toastMaster({
+						type: "errorDark",
+						message: "Error sending email",
+						bg: "white",
+						position: "bottomLeft",
+						transition: "top",
+					});
+				}
+			);
 	};
 	return (
 		<div>
@@ -49,7 +69,7 @@ const Contacts = () => {
 							<input
 								type="text"
 								name="from_name"
-								className="rounded-sm shadow-md focus:outline-0 text-slate-300 bg-black/40"
+								className="px-2 py-1 text-white rounded-sm shadow-md focus:outline-0 dark:text-gray-400 bg-black/40"
 							/>
 						</div>
 						<div className="flex flex-col justify-start gap-y-1">
@@ -57,13 +77,13 @@ const Contacts = () => {
 							<input
 								type="email"
 								name="user_email"
-								className="rounded-sm shadow-md focus:outline-0 bg-black/40 text-slate-300"
+								className="px-2 py-1 text-white rounded-sm shadow-md focus:outline-0 bg-black/40 dark:text-gray-400"
 							/>
 						</div>
 						<div className="flex flex-col justify-start gap-y-1">
 							<label>Subject</label>
 							<input
-								className="rounded-sm shadow-md focus:outline-0 text-slate-300 bg-black/40"
+								className="px-2 py-1 text-white rounded-sm shadow-md focus:outline-0 dark:text-gray-400 bg-black/40"
 								type="text"
 								name="user_subject"
 							/>
@@ -72,17 +92,17 @@ const Contacts = () => {
 							<label>Message</label>
 							<textarea
 								name="message"
-								rows="8"
-								className="text-sm rounded-sm shadow-md focus:outline-0 bg-black/40 text-slate-300"
+								rows="6"
+								className="px-2 py-1 text-white rounded-sm shadow-md focus:outline-0 bg-black/40 dark:text-gray-400"
 							/>
 						</div>
 						<Slide direction="up">
 							<div className="flex justify-end mt-3">
 								{/* <input
-							type="submit"
-							value="Send"
-							className="flex items-center justify-center px-6 py-2 font-sans text-lg font-medium text-white duration-300 border-t border-gray-900 rounded-sm shadow-md cursor-pointer hover:bg-red-600 dark:hover:bg-red-600 whitespace-nowrap gap-x-2 focus:outline-0 dark:border-white/20 bg-dhusor dark:bg-gray-950"
-						/> */}
+									type="submit"
+									value="Send"
+									className="flex items-center justify-center px-6 py-2 font-sans text-lg font-medium text-white duration-300 border-t border-gray-900 rounded-sm shadow-md cursor-pointer hover:bg-red-600 dark:hover:bg-red-600 whitespace-nowrap gap-x-2 focus:outline-0 dark:border-white/20 bg-dhusor dark:bg-gray-950"
+								/> */}
 								<button
 									type="submit"
 									className="flex items-center justify-center px-6 py-2 font-sans text-lg font-medium text-white duration-300 border-t border-gray-900 rounded-sm shadow-md cursor-pointer hover:bg-red-600 dark:hover:bg-red-600 whitespace-nowrap gap-x-2 focus:outline-0 dark:border-white/20 bg-dhusor dark:bg-gray-950"

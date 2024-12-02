41db6d08db834f1174c50accb68780b4f38a6d83
mk-saadi <mksaadi820@gmail.com>
2024-12-02 21:28:26 +0600

    minor changes

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -10,45 +10,57 @@ const Contacts = () => {
 
 	const sendEmail = (e) => {
 		e.preventDefault();
+		console.log(form);
 
-		toastMaster({
-			type: "loadingDark",
-			message: "Sending email",
-			footer: "Please wait, it might take a few seconds",
-			bg: "white",
-			position: "bottomLeft",
-			transition: "top",
-		});
-
-		emailjs
-			.send(
-				import.meta.env.VITE_SERVICE,
-				import.meta.env.VITE_TEMPLATE,
-				form.current,
-				import.meta.env.VITE_ID
-			)
-			.then(
-				(result) => {
-					e.target.reset();
-					toastMaster({
-						type: "successDark",
-						message: "Email sent successfully",
-						bg: "white",
-						position: "bottomLeft",
-						transition: "top",
-					});
-				},
-				(error) => {
-					toastMaster({
-						type: "errorDark",
-						message: "Error sending email",
-						bg: "white",
-						position: "bottomLeft",
-						transition: "top",
-					});
-				}
-			);
+		// emailjs
+		// 	.sendForm(import.meta.env.VITE_SERVICE, import.meta.env.VITE_TEMPLATE, form.current, {
+		// 		publicKey: import.meta.env.VITE_ID,
+		// 	})
+		// 	.then(
+		// 		() => {
+		// 			toastMaster({
+		// 				type: "successDark",
+		// 				message: "Email sent successfully",
+		// 			});
+		// 		},
+		// 		(error) => {
+		// 			toastMaster({
+		// 				type: "errorDark",
+		// 				message: "Error sending email",
+		// 			});
+		// 		}
+		// 	);
 	};
+
+	// emailjs
+	// 	.send(
+	// 		import.meta.env.VITE_SERVICE,
+	// 		import.meta.env.VITE_TEMPLATE,
+	// 		form.current,
+	// 		import.meta.env.VITE_ID
+	// 	)
+	// 	.then(
+	// 		(result) => {
+	// 			e.target.reset();
+	// 			toastMaster({
+	// 				type: "successDark",
+	// 				message: "Email sent successfully",
+	// 				bg: "white",
+	// 				position: "bottomLeft",
+	// 				transition: "top",
+	// 			});
+	// 		},
+	// 		(error) => {
+	// 			toastMaster({
+	// 				type: "errorDark",
+	// 				message: "Error sending email",
+	// 				bg: "white",
+	// 				position: "bottomLeft",
+	// 				transition: "top",
+	// 			});
+	// 		}
+	// 	);
+
 	return (
 		<div>
 			<Fade
@@ -74,7 +86,7 @@ const Contacts = () => {
 							<label>Name</label>
 							<input
 								type="text"
-								name="from_name"
+								name="to_name"
 								className="px-2 py-1 text-white rounded-sm shadow-md focus:outline-0 dark:text-gray-400 bg-black/40"
 							/>
 						</div>
@@ -82,7 +94,7 @@ const Contacts = () => {
 							<label>Email</label>
 							<input
 								type="email"
-								name="user_email"
+								name="from_email"
 								className="px-2 py-1 text-white rounded-sm shadow-md focus:outline-0 bg-black/40 dark:text-gray-400"
 							/>
 						</div>
@@ -107,17 +119,17 @@ const Contacts = () => {
 							triggerOnce
 						>
 							<div className="flex justify-end mt-3">
-								{/* <input
+								<input
 									type="submit"
 									value="Send"
 									className="flex items-center justify-center px-6 py-2 font-sans text-lg font-medium text-white duration-300 border-t border-gray-900 rounded-sm shadow-md cursor-pointer hover:bg-red-600 dark:hover:bg-red-600 whitespace-nowrap gap-x-2 focus:outline-0 dark:border-white/20 bg-dhusor dark:bg-gray-950"
-								/> */}
-								<button
+								/>
+								{/* <button
 									type="submit"
 									className="flex items-center justify-center px-6 py-2 font-sans text-lg font-medium text-white duration-300 border-t border-gray-900 rounded-sm shadow-md cursor-pointer hover:bg-red-600 dark:hover:bg-red-600 whitespace-nowrap gap-x-2 focus:outline-0 dark:border-white/20 bg-dhusor dark:bg-gray-950"
 								>
 									<Forward /> Send
-								</button>
+								</button> */}
 							</div>
 						</Slide>
 					</form>
